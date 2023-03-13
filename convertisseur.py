1861f003e55777553f696b9530904136c379be9e
Ivanovitchs <ij.ngoudjo@gmail.com>
2023-03-13 16:51:21 +0100

    amélioration de l'application console

--- convertisseur.py
+++ convertisseur.py
@@ -1,22 +1,24 @@
-convertisseur=input("vous souhaitez convertir pouces vers cm ou cm vers pouces ? " )
-sortir=0
-if convertisseur=="pouces vers cm":
-    while sortir==0:
-        try:
-            cm=int(input("entrer la valeur à convertir (inch) " ))
-            converte=float(round(cm*2.54,2))
-            print(str(converte)+ " cm")
-            print("Appuyer sur Enter pour sortir")
-        except ValueError:
-            break;
-       
-elif convertisseur=="cm vers pouces":
-    while sortir==0:
-        try:
-            cm=int(input("entrer la valeur à convertir (cm) "))
-            converte=float(round(cm*0.394,2))
-            print(str(converte)+ " inch")
-            print("Appuyer sur Enter pour sortir")
-        except ValueError:
-            break;
-    
+
+def funct_convert(unite1,unite2,facteur):
+        cm= input("conversion de " + str(unite1) + " vers " + str(unite2) + ". Donnez la valeur en " + str(unite1 ) + ". appuyer sur la touche q pour terminer " )
+        if cm=="q":
+            return True
+        cms=float(cm)
+        converte=float(round(cms*facteur,2))
+        print("le resultats de la conversion est : " + str(converte))
+        return False
+
+print("convertion des nombres")
+print("taper 1 pour la convertion des pouces vers cm")
+print("taper 2 pour la convertion des cm vers pouces")
+choix=input("entrer votre choix " )
+
+if choix=="1":
+    while True:
+        if funct_convert("inch","cm",5.64):
+            break
+elif choix=="2":
+    while True:
+        if funct_convert("cm","inch",0.36):
+            break
+        
